fadd292c95afe4449f71ce2ae55b8d299f45259c
Trey Marshall <marshall.trey92@yahoo.com>
2022-11-10 14:42:47 -0600

    Complete functions.js and self eval

--- functions.js
+++ functions.js
@@ -1,13 +1,41 @@
 // 1: Write a function named printGreeting that prints a simple greeting message, the same one, every time it is called. Call this function 3 times.
 
+function printGreeting(name) {
+    console.log(`Hello, how are you doing ${name}?`)
+}
+printGreeting("Trey")
+printGreeting("Kelly")
+printGreeting("Bryan")
 
 // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
 
+function friendlyGesture(gesture) {
+    console.log(`*${gesture}*`)
+}
+friendlyGesture("Wave")
 
 // 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
 
+function companyMinMax(company, num1, num2){
+    console.log(`The pay range at ${company} is ${num1 - num2}`)    
+}
+
+companyMinMax("Walmart", 110000, 90000)
+
+
+
 
 // 4: Write a function that satifies the following interaction pattern:
+
+function checkStock(stock, item) {
+    if (stock === 0) {
+    console.log(`${item} - Out of stock!`) 
+    } else if (stock < 4) {
+    console.log(`${item} - running LOW`)
+    } else {
+      console.log(`${item} is stocked`)
+    }
+}
 
 checkStock(4, "Coffee");
 // => "Coffee is stocked"
@@ -16,6 +44,7 @@ checkStock(3, "Tortillas");
 // => "Tortillas - running LOW"
 
 checkStock(0, "Cheese");
+
 // => "Cheese - OUT of stock!"
 
 checkStock(1, "Salsa");
--- selfEval.md
+++ selfEval.md
@@ -1,6 +1,19 @@
 # Self Evaluation
 
 - Reflect on your time management and awareness of where you spend time. Is this a strength or area you are working on? What support do you need in this area? And/or, what tips do you have to share with your classmates in this area?
+
+I feel I need to improve on time management as I do over analyze the content and lead myself in circles. I generally seek support from my peers when I am able, as it cleans up my understanding. A tip would be to not try to over think everything.
+
 - How would you explain to a 5 year old what a function is?
+
+A function works like follow the leader where the function is the leader. Or a function is like Simon says where the function is Simon and you have to listen to it.
+
 - Share your understanding of arguments and parameters.
+
+An argument is the actual data that is being used to display code for a function.
+
+A parameter is a placeholder that is given a value that just acts as a place to start from.
+
 - What questions/topics (related to Functions) do you want to being to your Small Group?
+
+Probably more about numbers in a function. That would probably be the biggest area to discuss.
